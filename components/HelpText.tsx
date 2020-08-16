2f6a57463db04487d6d9c7fafb7b117286853aef
Vikas Gorur <vikas@airbase.io>
2020-08-16 15:52:13 +0530

    Add help text

--- components/HelpText.tsx
+++ components/HelpText.tsx
@@ -2,6 +2,11 @@ import styles from "./HelpText.module.css";
 
 export default function HelpText(props) {
     return <div className={styles.help_text}>
-        How to use this?
+        This is an app that helps you post a quote from a book to Twitter.
+        <ol className="list-decimal list-inside">
+            <li className={styles.list_item}>Click on the text and type in your quote.</li>
+            <li className={styles.list_item}>Edit the author, etc. if the auto-fill was wrong.</li>
+            <li className={styles.list_item}>Click "Copy" to copy the image; paste it into Twitter.</li>
+        </ol>
     </div>
 }
